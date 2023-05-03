090eca7a91c1e1bf24c939e10e4a15e966f8a6d8
Cassandra Reinhart <cassandrareinhart13@gmail.com>
2023-05-02 23:03:42 -0500

    refactored to class-based components

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,74 +1,174 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
 import Joke from "./Joke";
 import "./JokeList.css";
 
-function JokeList({ numJokesToGet = 10 }) {
-  const [jokes, setJokes] = useState([]);
+class JokeList extends React.Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  }
 
-  /* get jokes if there are no jokes */
-
-  useEffect(function() {
-    async function getJokes() {
-      let j = [...jokes];
-      let seenJokes = new Set();
-      try {
-        while (j.length < numJokesToGet) {
-          let res = await axios.get("https://icanhazdadjoke.com", {
-            headers: { Accept: "application/json" }
-          });
-          let { status, ...jokeObj } = res.data;
-  
-          if (!seenJokes.has(jokeObj.id)) {
-            seenJokes.add(jokeObj.id);
-            j.push({ ...jokeObj, votes: 0 });
-          } else {
-            console.error("duplicate found!");
-          }
-        }
-        setJokes(j);
-      } catch (e) {
-        console.log(e);
-      }
+  constructor(props) {
+    super(props);
+    this.state = {
+      jokes: [],
+      jokeVotes: {}
     }
-
-    if (jokes.length === 0) getJokes();
-  }, [jokes, numJokesToGet]);
-
-  /* empty joke list and then call getJokes */
-
-  function generateNewJokes() {
-    setJokes([]);
+    this.generateNewJokes = this.generateNewJokes.bind(this);
+    this.vote = this.vote.bind(this);
   }
 
-  /* change vote for this id by delta (+1 or -1) */
-
-  function vote(id, delta) {
-    setJokes(allJokes =>
-      allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
-    );
+  componentDidMount() {
+    if (this.state.jokes.length < this.props.numJokesToGet) {
+      this.getJokes();
+    }
   }
 
-  /* render: either loading spinner or list of sorted jokes. */
+  componentDidUpdate() {
+    if (this.state.jokes.length < this.props.numJokesToGet) {
+      this.getJokes();
+    }
+  }
 
-  if (jokes.length) {
-    let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
-  
+  async getJokes() {
+    try {
+      let jokes = this.state.jokes;
+      let jokeVotes = this.state.jokeVotes;
+      let seenJokes = new Set(jokes.map(j => j.id));
+
+      while (jokes.length < this.props.numJokesToGet) {
+        let res = await axios.get("https://icanhazdadjoke.com", {
+          headers: { Accept: "application/json" }
+        });
+        let { status, ...joke } = res.data;
+
+        if (!seenJokes.has(joke.id)) {
+          seenJokes.add(joke.id);
+          jokeVotes[joke.id] = jokeVotes[joke.id] || 0;
+          jokes.push({ ...joke, votes: jokeVotes[joke.id]});
+        } else {
+          console.log("duplicate found!");
+        }
+      }
+
+      this.setState({ jokes });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  generateNewJokes() {
+    this.setState(s => ({ jokes: []}));
+  }
+
+  vote(id, delta) {
+    jokeVotes[id] = (jokeVotes[id] || 0) + delta;
+    
+    this.setState(s => ({
+      jokes: s.jokes.map(j =>
+        j.id === id ? { ...j, votes: j.votes + delta } : j
+      )
+    }));
+  }
+  // function vote(id, delta) {
+    //     setJokes(allJokes =>
+    //       allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+    //     );
+    //   }
+
+  render() {
+    let sortedJokes = [...this.state.jokes].sort((a, b) => b.votes - a.votes);
+    
+
     return (
       <div className="JokeList">
-        <button className="JokeList-getmore" onClick={generateNewJokes}>
+        <button
+          className="JokeList-getmore"
+          onClick={this.generateNewJokes}
+        >
           Get New Jokes
         </button>
-  
+
         {sortedJokes.map(j => (
-          <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
+          <Joke
+            text={j.joke}
+            key={j.id}
+            id={j.id}
+            votes={j.votes}
+            vote={this.vote}
+          />
         ))}
       </div>
     );
   }
-
-  return null;
-
 }
 
 export default JokeList;
+//   const [jokes, setJokes] = useState([]);
+
+//   /* get jokes if there are no jokes */
+
+//   useEffect(function() {
+//     async function getJokes() {
+//       let j = [...jokes];
+//       let seenJokes = new Set();
+//       try {
+//         while (j.length < numJokesToGet) {
+//           let res = await axios.get("https://icanhazdadjoke.com", {
+//             headers: { Accept: "application/json" }
+//           });
+//           let { status, ...jokeObj } = res.data;
+  
+//           if (!seenJokes.has(jokeObj.id)) {
+//             seenJokes.add(jokeObj.id);
+//             j.push({ ...jokeObj, votes: 0 });
+//           } else {
+//             console.error("duplicate found!");
+//           }
+//         }
+//         setJokes(j);
+//       } catch (e) {
+//         console.log(e);
+//       }
+//     }
+
+//     if (jokes.length === 0) getJokes();
+//   }, [jokes, numJokesToGet]);
+
+//   /* empty joke list and then call getJokes */
+
+//   function generateNewJokes() {
+//     setJokes([]);
+//   }
+
+//   /* change vote for this id by delta (+1 or -1) */
+
+//   function vote(id, delta) {
+//     setJokes(allJokes =>
+//       allJokes.map(j => (j.id === id ? { ...j, votes: j.votes + delta } : j))
+//     );
+//   }
+
+//   /* render: either loading spinner or list of sorted jokes. */
+
+//   if (jokes.length) {
+//     let sortedJokes = [...jokes].sort((a, b) => b.votes - a.votes);
+  
+//     return (
+//       <div className="JokeList">
+//         <button className="JokeList-getmore" onClick={generateNewJokes}>
+//           Get New Jokes
+//         </button>
+  
+//         {sortedJokes.map(j => (
+//           <Joke text={j.joke} key={j.id} id={j.id} votes={j.votes} vote={vote} />
+//         ))}
+//       </div>
+//     );
+//   }
+
+//   return null;
+
+// }
+
+// export default JokeList;
